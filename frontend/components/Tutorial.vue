50c4eb9382de61bd3fa80faa7190c6aad684f17d
gureenkov56 <gureenkov56@gmail.com>
2023-05-14 21:18:18 +0500

    first commit

---
+++ frontend/components/Tutorial.vue
@@ -0,0 +1,43 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <h1>NUXT успешно запущен</h1>
+
+    <h2>Данные получены от сервера:</h2>
+    <ul>
+      <li 
+        v-for="(item, idx) in list"
+        :key="idx"
+      >
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref } from 'vue'
+import axios from 'axios'
+
+export default defineComponent({
+  name: 'Tutorial',
+  setup() {
+    const list = ref([])
+
+    onMounted(() => {
+      axios
+        .get('/api/list')
+        .then((res) => (list.value = res.data))
+      // fetch('/list')
+      //   .then(data => data.json())
+      //   .then(data => list.value = data);
+    })
+
+    return {
+      list,
+    }
+  },
+})
+</script>
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('port') || 5000
+
+app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'frontend', 'dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/list', (req, res) => {
+    // res.header("Access-Control-Allow-Origin", "*");
+    res.send(['Pit', 'Tim', "Bim"]);
+})
+
+app.listen(PORT, () => {
+    if (process.env.NODE_ENV === 'production') {
+        console.log('PRODUCTION MODE');
+    }
+
+    console.log(`Example app listening on: http://localhost:${PORT}/`)
+})
